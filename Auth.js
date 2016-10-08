2524ab99d201d8c67a3079bb306218b390115c70
Edouard FERRARI <epi.doud@gmail.com>
2016-10-08 16:32:24 +0000

    bug(Auth): Redirect chain go through 500

--- Auth.js
+++ Auth.js
@@ -1,5 +1,6 @@
 var request           = require('request');
 var Logger            = require('node-wrapper/logger')
+var url               = require('url')
 
 var jar               = require("./common/jar.js");
 
@@ -15,14 +16,12 @@ var Auth = function (config) {
         , "FacebookConnectTries": 0
         , "Password": config.auth.password
     };
-    var url = "https://secure.fr.vente-privee.com/authentication/login/FR?ReturnUrl=/vp4/Home/Default.aspx";
-
 
     this.login = (callback) => {
         var options = {};
         
         options = {
-            uri: url
+            url: "https://secure.fr.vente-privee.com/authentication/login/FR?ReturnUrl=/vp4/Home/Default.aspx"
             , method: 'POST'
             , json: data
             , jar: jar.get(config)
@@ -31,12 +30,30 @@ var Auth = function (config) {
                 "Upgrade-Insecure-Requests": 1
             }
         }; 
+
+        var requestAfter = (error, response, body) => {
+            // manage redirection
+            if (300 <= response.statusCode && response.statusCode < 400) {
+                var locationUrl = url.parse(response.headers.location);
+                var originUrl = url.parse(options.url);
+
+                if (locationUrl.host == undefined || locationUrl.host.length < 1) {
+                    originUrl.pathname = locationUrl.pathname;
+                } else {
+                    originUrl = locationUrl;
+                }
+                
+                options.url = url.format(originUrl);
+                
+                logger.log("(%s) %s", response.statusCode, options.url);
+                return request(options, requestAfter);
+            } else {
+                console.log(response.headers);
+                return callback(error);
+            }
+        };
         
-        request(options, function (error, response, body) {
-            logger.log("StatusCode: %s", response.statusCode);
-            console.log(response.headers);
-            return callback(error);
-        });
+        request(options, requestAfter);
     };
 };
 
