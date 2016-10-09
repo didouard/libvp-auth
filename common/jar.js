552a45476adbe2b63b2b6f19c85d7f4e98ac5802
Edouard FERRARI <epi.doud@gmail.com>
2016-10-09 19:35:25 +0000

    todo(Jar): Manage creating file on first launch

--- common/jar.js
+++ common/jar.js
@@ -2,6 +2,17 @@ var request                     = require("request");
 var FileCookieStore             = require('tough-cookie-filestore');
 
 var Jar = function () {
+        
+    // todo
+    var openJarFile = (callback) => {
+        fs.open(config.cookies['jar-file'], 'w', callback);
+    };
+    
+    var closeJarFile = (fd, callback) => {
+        fs.close(fd, callback);
+    };
+    //!todo
+    
     var jar = [];
     this.get = (config) => {
         if (jar[config.cookies['jar-file']] == undefined) 
